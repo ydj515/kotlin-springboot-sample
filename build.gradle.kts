62d4540c45461e1595ba80c25ae96d46ebacb660
ydj515 <ydj515@hanmail.net>
2024-11-21 17:21:04 +0900

    feat: add kotest & mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,19 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Kotest 의존성
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
+    // MockK 의존성
+    testImplementation("io.mockk:mockk:1.13.7")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+
 }
 
 kotlin {
