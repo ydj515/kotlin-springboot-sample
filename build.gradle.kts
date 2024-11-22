d92eaf37022f343f0bcafb4e0c2402a5392e5afd
ydj515 <ydj515@hanmail.net>
2024-11-23 00:08:35 +0900

    fix: http response header값 설정 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
