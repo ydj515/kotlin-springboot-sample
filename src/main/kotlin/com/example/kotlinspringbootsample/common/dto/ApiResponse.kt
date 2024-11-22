22ffd07a3b738478b792a59744c146d4e97e6b61
ydj515 <ydj515@hanmail.net>
2024-11-22 22:38:03 +0900

    refactor: 불필요 코드 제거

--- src/main/kotlin/com/example/kotlinspringbootsample/common/dto/ApiResponse.kt
+++ src/main/kotlin/com/example/kotlinspringbootsample/common/dto/ApiResponse.kt
@@ -3,21 +3,14 @@ package com.example.kotlinspringbootsample.common.dto
 import java.time.LocalDateTime
 
 data class ApiResponse<T>(
+    val success: Boolean = true,
     val code: String,
     val message: String,
     val timestamp: LocalDateTime = LocalDateTime.now(),
     val data: T?
 ) {
     companion object {
-        fun <T> success(data: T, resultCode: ResultCode): ApiResponse<T> {
-            return ApiResponse(
-                code = resultCode.code,
-                message = resultCode.message,
-                data = data
-            )
-        }
-
-        fun <T> error(resultCode: ResultCode, data: T? = null): ApiResponse<T> {
+        fun <T> of(data: T, resultCode: ResultCode): ApiResponse<T> {
             return ApiResponse(
                 code = resultCode.code,
                 message = resultCode.message,
